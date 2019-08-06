c670d75f92c55fc974f412b27385397bb8a62264
Peter Stanko <pstanko@netsuite.com>
2019-08-06 15:24:21 +0200

    Updated in lunch - better loadable and serializable

--- pylunch/lunch.py
+++ pylunch/lunch.py
@@ -1,10 +1,11 @@
 import logging
-from typing import List, Optional, Mapping, Tuple
+from typing import List, Optional, Mapping, Tuple, Union, Any, MutableMapping
 
 import html2text
 import requests
 import yaml
 import datetime
+import collections.abc
 from bs4 import BeautifulSoup, Tag
 from requests import Response
 
@@ -16,45 +17,90 @@ log = logging.getLogger(__name__)
 RHLP_URL="http://rhlp-oskutka.8a09.starter-us-east-2.openshiftapps.com"
 
 class LunchEntity:
-    def __init__(self, name: str, url: str=None, display_name: str=None, tags: List[str]=None, selector: str=None, request_params: List[str]=None):
-        self._name = name
-        self._url = url
-        self._selector = selector
-        self._request_params = request_params or {}
-        self._display_name = display_name
+    def __init__(self, config: Mapping[str, Any]):
+        self._config = {**config}
         self.tags = tags or []
+
+    def __getitem__(self, k):
+         return self._config.get(k)
+    def __setitem__(self, k, v):
+         self.config[k] = v
+    def __delitem__(self, k):
+         del self.config[k]
+    def __iter__(self):
+        return iter(self._config)
+    def __len__(self):
+         return len(self.config)
+
+    @property
+    def config(self) -> MutableMapping['str', Any]:
+        return self._config
 
     @property
     def name(self) -> str:
-        return self._name
-
-    @property
-    def display_name(self) -> str:
-        return self._display_name or self.name
+        return self._config['name']
 
     @property
     def url(self) -> str:
-        return self._url
+        return self.config.get('url')
+
+    @property
+    def selector(self) -> str:
+        return self.config.get('selector')
+
+    @property
+    def request_params(self) -> List:
+        return self.config.get('request_params', {})
+
+    @property
+    def display_name(self) -> str:
+        return self._config.get('display_name') or self.name
+
+    @property
+    def tags(self) -> List[str]:
+        return self._config.get('tags')
+
+    def __str__(self) -> str:
+        result = f"{self.__class__.__name__} \"{self.name}\""
+
+        if self.display_name:
+            result += f" ({self.display_name})"
+        if self.tags:
+            result += f" - {self.tags}"
+        
+        result += f" {self.url}"
+
+        if self.selector:
+            result += f" ({self.selector})"
+        if self._request_params:
+            result += f" - req_params={self._request_params}"
+        return result
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class LunchResolver:
+    def __init__(self, service: LunchService, entity: LunchEntity):
+        self.service = service
+        self.entity = entity
 
     def make_request(self, **kwargs) -> requests.Response:
-        response = requests.get(self._url, **kwargs)
+        response = requests.get(self.entity.url, **kwargs)
         if not response.ok:
-            log.warning(f"[LUNCH] Error: {response.content}")
+            log.warning(f"[LUNCH] Error[{response.status_code}]: {response.content}")
+            print(f"Error[{response.status_code}]: ", response.content)
         return response
 
-    def get_soap(self, response: Response) -> BeautifulSoup:
-        soup = BeautifulSoup(response.content, "lxml")
-        return soup
-
-    def parse(self, response: Response) -> List[Tag]:
-        soap = self.get_soap(response)
-        sub = soap.select(self._selector) if self._selector else soap
+    def parse_response(self, response: Response) -> List[Tag]:
+        soap = BeautifulSoup(response.content, "lxml")
+        sub = soap.select(self.selector) if self.selector else soap
         return sub
 
     def invoke(self) -> str:
         response = self.make_request(**self._request_params)
-        parsed = self.parse(response=response)
-        log.info(f"[LUNCH] Parsed[{self._name}]: {parsed}")
+        parsed = self.parse_response(response=response)
+        log.info(f"[LUNCH] Parsed[{self.entity.name}]: {parsed}")
         string = self.to_string(parsed)
         return to_text(string)
 
@@ -66,26 +112,7 @@ class LunchEntity:
             return "".join(items)
         else:
             return parsed.extract()
-
-    def __str__(self) -> str:
-        result = f"{self.__class__.__name__} \"{self._name}\""
-
-        if self.display_name:
-            result += f" ({self.display_name})"
-        if self.tags:
-            result += f" - {self.tags}"
-        
-        result += f" {self._url}"
-
-        if self._selector:
-            result += f" ({self._selector})"
-        if self._request_params:
-            result += f" - req_params={self._request_params}"
-        return result
-
-    def __repr__(self) -> str:
-        return str(self)
-
+    
 
 class RHLPLunch(LunchEntity):
     def __init__(self, name: str, url: str = None, selector=None, request_params=None, **kwargs):
@@ -93,22 +120,28 @@ class RHLPLunch(LunchEntity):
         super().__init__(name, url=f"{RHLP_URL}/{url_name}", **kwargs)
 
 
-class LunchService:
-    def __init__(self):
-        self._instances: Mapping[str, LunchEntity] = {}
-        self._providers: Mapping[str, type] = dict(default=LunchEntity, rhlp=RHLPLunch)
+class Resolvers:
+    def __init__(self, **kwargs):
+        self._collection = {**kwargs}
     
-    @property
-    def instances(self) -> Mapping[str, LunchEntity]:
-        return self._instances
+    def register(self, name: str, cls: type):
+        log.info(f"[ADD] Resolver [{name}]: {cls.__name__}")
+        self._collection[name] = cls
+
+    def get(self, name: str) -> type:
+        return self._collection.get(name, LunchResolver)
+
+class Entities:
+    def __init__(self):
+        self._entities: MuttableMapping[str, LunchEntity] = {}
 
     @property
-    def register_provider(self, name: str, cls: type):
-        log.info(f"[ADD] Provider [{name}]: {cls.__name__}")
-        self._providers[name] = cls
+    def entities(self) -> MuttableMapping[str, LunchEntity]:
+        return self._entities
 
+    
     def get(self, name: str) -> Optional[LunchEntity]:
-        if name in self.instances.keys():
+        if name in self.entities.keys():
             instance = self.instances[name]
             log.info(f"[LUNCH] Found in instances {name}: {instance}")
             return instance
@@ -126,43 +159,86 @@ class LunchService:
         return [i[0] for i in self.fuz_find(name, limit)]
 
     def fuz_find(self, name: str, limit=10) -> List[Tuple]:
-        return process.extract(name, self.instances,
+        return process.extract(name, self.entities,
                                processor=lambda x: x if isinstance(x, str) else x.name,
                                scorer=fuzz.token_sort_ratio,
                                limit=limit)
 
     def fuz_find_one(self, name: str) -> Tuple:
-        return process.extractOne(name, self.instances,
+        return process.extractOne(name, self.entities,
                                   processor=lambda x: x if isinstance(x, str) else x.name,
                                   scorer=fuzz.token_sort_ratio)
 
-    def register(self, name, url, display_name=None, tags=None, selector=None, request_params=None, cls=LunchEntity):
-        instance = cls(name=name, url=url, selector=selector, display_name=display_name, tags=tags, request_params=request_params)
+    def register(self, name, url, display_name=None, tags=None, selector=None, request_params=None, **kwargs):
+        config = dict(name=name, url=url, selector=selector, display_name=display_name, tags=tags, request_params=request_params, **kwargs)
+        instance = LunchEntity(config)
         log.info(f"[REG] Register: {instance}")
-        self.instances[name] = instance
+        self.entities[name] = instance
+
+class LunchService:
+    def __init__(self, config_dir: Union[str, Path]):
+        self._instances: Entities = Entities()
+        self._resolvers: Resolvers = Resolvers(default=LunchResolver, rhlp=RHLPLunch)
+        self._config_dir: Path = Path(config_dir) if config_dir is not None else None
+        self.config: Optional[str, Any] = None
+    
+    @property
+    def instances(self) -> Entities:
+        return self._instances
+
+    @property
+    def restaurants_file(self) -> Path:
+        return self.config_dir / 'restaurants.yaml'
+
+    @property
+    def config_file(self) -> Path:
+        return self.config_dir / 'config.yaml'
+    
+    @property
+    def config_dir(self) -> Mapping[str, Any]:
+        return self._config_dir
 
     
-    def register_from_file(self, file: Tuple[Path, str]):
+    def import_file(self, file: Tuple[Path, str]):
         file = Path(file)
+        if not file.exists():
+            log.warning(f"[IMPORT] File not exists: {file}")
+            return 
         with file.open("r") as fp:
             restaurants = yaml.safe_load(fp)
             for (name, restaurant) in restaurants['restaurants'].items():
-                self.instances[name] = self.create_restaurant(name, restaurant)
+                self.instances.register(**restaurant)
+    
+    def load_config(self, file: Path):
+        file = Path(file)
+        if not file.exists():
+            log.warning(f"[LOAD] Config file not exists: {file}")
+            return
+        with file.open("r") as fp:
+            self.config.update()
+            
 
-    def create_restaurant(self, name: str, restaurant: dict) -> LunchEntity:
-        cls_name = 'default'
-        if 'cls' in restaurant:
-            cls_name = restaurant['cls']
-            del restaurant['cls']
-        cls = self._get_provider(cls_name)
-        restaurant['name'] = name
-        return cls(**restaurant)
+    def load(self):
+        self.import_file(self.restaurants_file)
+        self.load_config(self.config_file)    
+
+    def save(self):
+        if self.config_dir and not self.config_dir.exists():
+            self.config_dir.mkdir(parents=True)
+
+        with self.config_file.open("w") as fp:
+            yaml.safe_dump(self.config, fp)
+
+        with self.restaurants_file.open("w") as fp:
+            data = dict()
+            for (name, entity) in self.instances:
+                yaml.safe_dump(dict(restaurants=data), fp)
                     
     def process_lunch_name(self, name: str) -> str:
         if not name or name == 'list':
             return self.to_string()
         log.info(f"[CMD] Lunch name: {name}")
-        instance = get(name)
+        instance = self.instances.get(name)
 
         try:
             content = f"Restaurant: \"{name}\" - {instance.url}\n" + instance.invoke()
@@ -184,13 +260,9 @@ class LunchService:
         return entity.invoke()
 
 
-    def _get_provider(self, name: str) -> type:
-        return self._providers.get(name) or self._providers['default']
-
-
 class CachedLunchService(LunchService):
-    def __init__(self, cache_base=None):
-        super().__init__()
+    def __init__(self, config_dir: Union[str, Path], cache_base=None):
+        super().__init__(config_dir)
         self.cache_base = Path(cache_base) if cache_base else None
     
     def _create_cache_for_day(self, day: str=None) -> Path:
@@ -220,7 +292,7 @@ class CachedLunchService(LunchService):
         if file is not None and file.exists():
             log.debug(f"[CACHE] Cache hit for {entity.name}: {file}")
             return file.read_text(encoding='utf-8')
-            
+
         content = entity.invoke()
         if self.cache_base is not None:
             self._create_cache_for_day()
