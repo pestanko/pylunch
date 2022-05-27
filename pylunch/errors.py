b57b4787e6156057fe81a477acbdf76273caeb6d
Peter Stanko <peter.stanko0@gmail.com>
2022-05-27 09:05:49 +0200

    Fix the Error class name, add the url paramter

--- pylunch/errors.py
+++ pylunch/errors.py
@@ -13,9 +13,10 @@ class PyLunchApiError(PyLunchError):
         return dict(message=self.message, status=self.code)
 
 
-class UnnableToLoadContent(PyLunchApiError):
-    def __init__(self, name, code=400):
-        super().__init__(f"Cannot parse the menu for: {name}")
+class UnableToLoadContent(PyLunchApiError):
+    def __init__(self, name: str, code=400, url: str = None):
+        super().__init__(f"""Cannot parse the menu for: {name}\n
+        You can try to take a look at here: {url}""", code=code)
         self.name = name
 
     def to_json(self):
