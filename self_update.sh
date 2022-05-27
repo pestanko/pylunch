cc68decf947bae5037bdb70406ce874a312221a9
Peter Stanko <peter.stanko0@gmail.com>
2022-05-27 09:47:34 +0200

    Fix the poetry is not on the path

--- self_update.sh
+++ self_update.sh
@@ -1,11 +1,15 @@
 #! /bin/bash 
 
-PROJECT_ROOT = "${1}"
+PROJECT_ROOT="${1}"
 
 echo "Project ROOT: ${PROJECT_ROOT}"
 
-cd ${PROJECT_ROOT}
+cd "${PROJECT_ROOT}"
 git pull --force origin master
+
+if [ -e "$HOME/.poetry/bin" ]; then
+  export PATH="$HOME/.poetry/bin:$PATH"
+fi
 
 poetry install
 
